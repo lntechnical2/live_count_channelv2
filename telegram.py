b54a0bda4aa924d362b89c29a44073cfbaac8983
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-08-29 15:33:04 +0530

    Create telegram.py

---
+++ telegram.py
@@ -0,0 +1,43 @@
+import requests as re 
+import json 
+from typing import Union
+
+class lnbot:
+	def __init__(self, token: str):
+		self.api = "https://api.telegram.org/bot"+token+"/"
+		
+		
+	def send_message(self,chat_id :Union[int,str]  , text: str ):
+		send_api = self.api+"sendMessage"
+		data = { 
+		   "chat_id": chat_id,
+		   "text":text		
+		   }
+		response = re.post(send_api,data)
+		
+	
+	def edit(self,
+	       chat_id: Union[int,str] , 
+	       message_id : int , 
+	       text : str ,
+	       disable_webpage : bool= None,
+	       parse_mode :str = None  ):
+	        	edit_api = self.api +"editMessageText"
+	        	data = {
+	        	"chat_id": chat_id,
+	        	"message_id" : message_id,
+	        	"text": text,
+	        	"parse_mode" : parse_mode,
+	        	"disable_web_page_preview" : disable_webpage	        	
+	        	}
+	        	response = re.post(edit_api,data)
+	        	
+	        	
+	
+	def count(self , chat_id: str):
+		 count_api = self.api +"getChatMembersCount"
+		 data = { "chat_id": chat_id
+		 }
+		 res = re.post(count_api,data)
+		 value = res.json()
+		 return value["result"]
